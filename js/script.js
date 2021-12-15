e389f2060b93b5b87eae40a8f9d9a864739e1b87
Deki97 <dejan97@libero.it>
2021-12-15 16:01:16 +0100

    generazione mail

--- js/script.js
+++ js/script.js
@@ -13,13 +13,71 @@ const app = new Vue(
     {
         el: '#root',
         data: {
-
+            emailArray: [],
         },
         methods: {
 
         },
         created: function() {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
+
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((response) => {
+                // console.log(response.data.response);
+                this.emailArray.push(response.data.response);
+            });
         }
     }
 );
